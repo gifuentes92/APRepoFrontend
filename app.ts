7f8ea639d2e69857752042ec5c04bce001647bbe
gifuentes92 <isa2_7_92@hotmail.com>
2022-03-23 20:01:28 -0300

    0.4
    -Funcionalidad de algunos botones de "editar" añadido con js
    
    -División de algunas class en "id" para que puedan funcionar los botones
    
    -Prueba de typescript y creacion del archivo json

---
+++ app.ts
@@ -0,0 +1,3 @@
+var saludo:string="Hola mundo";
+console.log(saludo);
+
--- arriba.js
+++ arriba.js
@@ -1,8 +1,9 @@
-//Get the button:
+"use strict";
+
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+//window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -17,3 +18,20 @@ function topFunction() {
   document.body.scrollTop = 0; // Safari
   document.documentElement.scrollTop = 0; //  Chrome, Firefox, IE and Opera
 }
+
+
+function cambiarInfoAEditar()
+{
+  let cambiarInfoAEditarInput=prompt("Ingrese el nuevo valor a editar");
+  document.getElementById("parrafo").innerHTML=cambiarInfoAEditarInput;
+}
+
+
+function cambiarNombre()
+{
+  let cambiarNombreInput=prompt("Ingrese el nombre");
+  document.getElementById("titulo").innerHTML=cambiarNombreInput;
+}
+
+
+
